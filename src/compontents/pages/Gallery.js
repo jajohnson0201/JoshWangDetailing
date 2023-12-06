db96c0a53243a1f06a48523d31cdf6fc5d0a60cb
joe johnson <jajohnson0201@gmail.com>
2023-12-06 16:42:29 -0600

    added a carouseland cards for it to the Gallery page...

--- src/compontents/pages/Gallery.js
+++ src/compontents/pages/Gallery.js
@@ -1,9 +1,53 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+
 
 export default function Gallery (){
     return (
+        
         <div>
-            <h3>Gallery</h3>
-        </div>
+        <Carousel pause="hover" >
+        <Carousel.Item interval={3000} >
+
+        <Card style={{ width: '50rem' ,height: '30rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        </Carousel.Item>
+
+        <Carousel.Item interval={2000} >
+        <Card style={{ width: '50rem', height: '30rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        </Carousel.Item>
+
+        <Carousel.Item interval={5000}>
+        <Card style={{ width: '50rem' , height: '30rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        </Carousel.Item>
+      </Carousel>
+      </div>
     );
 }
